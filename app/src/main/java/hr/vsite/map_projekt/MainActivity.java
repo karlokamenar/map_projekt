4728e864814e7b4c48327f216da4184e13ad68ae
karlokamenar <karlokamenar1996@gmail.com>
2021-09-10 17:06:34 +0200

    commit1

---
+++ app/src/main/java/hr/vsite/map_projekt/MainActivity.java
@@ -0,0 +1,199 @@
+package hr.vsite.map_projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView convertFromDropdownTextView, convertToDropdownTextView, conversionRateText;
+    EditText amountToConvert;
+    ArrayList<String> arrayList;
+    Dialog fromDialog;
+    Dialog toDialog;
+    Button convertButton;
+    String  convertFromValue, convertToValue, conversionValue;
+    String[] drzava = {"AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTC","BTN","BWP","BYN","BYR","BZD",
+            "CAD","CDF","CHF","CLF","CLP","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF",
+            "GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR",
+            "LRD","LSL","LVL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR",
+            "PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS",
+            "UAH","UGX","USD","UYU","UZS","VEF","VND","VUV","WST","XAF","XAG","XCD","XDR","XOF","XPF","YER","ZAR","ZMK","ZMW","ZWL"
+    };
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        convertFromDropdownTextView = findViewById(R.id.convert_from_dropdown_menu);
+        convertToDropdownTextView = findViewById(R.id.convert_to_dropdown_menu);
+        convertButton = findViewById(R.id.conversionButton);
+        conversionRateText = findViewById(R.id.conversionRateText);
+        amountToConvert = findViewById(R.id.amountToConvertEditText);
+
+        arrayList = new ArrayList<>();
+        Collections.addAll(arrayList, drzava);
+
+        convertFromDropdownTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fromDialog = new Dialog(MainActivity.this);
+                fromDialog.setContentView(R.layout.forma_spinner);
+                fromDialog.getWindow().setLayout(750,900);
+                fromDialog.show();
+
+                EditText editText = fromDialog.findViewById(R.id.edit_text);
+                ListView listView = fromDialog.findViewById(R.id.list_view);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
+                listView.setAdapter(adapter);
+
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        adapter.getFilter().filter(s);
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+                listView.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        convertFromDropdownTextView.setText(adapter.getItem(position));
+                        fromDialog.dismiss();
+                        convertFromValue = adapter.getItem(position);
+                    }
+                });
+            }
+        });
+        convertToDropdownTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toDialog = new Dialog(MainActivity.this);
+                toDialog.setContentView(R.layout.to_spinner);
+                toDialog.getWindow().setLayout(750,900);
+                toDialog.show();
+
+                EditText editText = toDialog.findViewById(R.id.edit_text);
+                ListView listView = toDialog.findViewById(R.id.list_view);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
+                listView.setAdapter(adapter);
+
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        adapter.getFilter().filter(s);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        convertToDropdownTextView.setText(adapter.getItem(position));
+                        toDialog.dismiss();
+                        convertToValue = adapter.getItem(position);
+                    }
+                });
+            }
+        });
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Double amountToConvert = Double.valueOf(MainActivity.this.amountToConvert.getText().toString());
+                    getConversionRate(convertFromValue,convertToValue,amountToConvert);
+                }
+                catch (Exception ignored){
+
+                }
+            }
+        });
+
+
+    }
+
+    public void getConversionRate(String convertFrom, String convertTo, Double amountToConvert){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://free.currconv.com/api/v7/convert?q="+convertFrom+"_"+convertTo+"&compact=ultra&apiKey=5ff9dd5a2c59e4c1b88e";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject jsonObject;
+                try {
+                    jsonObject = new JSONObject(response);
+                    Double conversionRateValue = round(((Double) jsonObject.get(convertFrom + "_" + convertTo)), 2);
+                    conversionValue = "" + round((conversionRateValue * amountToConvert), 2);
+                    conversionRateText.setText(conversionValue);
+
+                } catch (JSONException e) {
+                    //e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    public static double round(double value, int places){
+        if(places<0) throw new IllegalArgumentException();
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+
+}
